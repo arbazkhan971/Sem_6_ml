edb1b1a40284594e4ca2e8c0ec80a2e4d4f05ab9
Arbaz Khan <arbazkhan971@gmail.com>
2018-12-19 01:11:57 +0530

    linear regression is updated and r2 sqaured featured is added

--- 1D_linear_regression.py
+++ 1D_linear_regression.py
@@ -2,4 +2,28 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+df = pd.read_csv("Data/data_1d.csv")
+X = df.iloc[:,0].values
+y = df.iloc[:,1].values
 
+
+plt.scatter(X,y)
+plt.show()
+
+
+den = X.dot(X) - X.mean()*X.sum()
+a = (y.dot(X) - y.mean() * X.sum())/den
+b = (y.mean() * X.dot(X) - X.mean() * y.dot(X))/den
+
+yhat = a*X + b
+
+
+plt.scatter(X,y)
+plt.plot(X,yhat, color="red")
+plt.show()
+
+
+d1 = y-yhat
+d2 = y - y.mean()
+r2 = 1 - d1.dot(d1)/d2.dot(d2)
+print("R-squared value is :{}".format(r2))
